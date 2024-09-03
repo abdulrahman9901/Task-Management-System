4a05a80a80607bc606115119913330c8f223508b
abdulrahman9901 <aba884682@gmail.com>
2024-09-03 15:55:41 +0200

    Fix:handling deployment issue - rewriting

--- middleware.ts
+++ middleware.ts
@@ -4,13 +4,13 @@ const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
 
 
 export default clerkMiddleware((auth, req) => {
-    if (!isPublicRoute(req)) {
-      auth().protect();
+    if (isPublicRoute(req)) {
+      return;
     }
+    auth().protect();
 });
 
 export const config = {
-  authorizedParties: ["https://custom.vercel.app", "http://localhost:3000"],
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
