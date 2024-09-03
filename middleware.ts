231e81060224eaea4dfd20fd6a01e274b942373f
abdulrahman9901 <aba884682@gmail.com>
2024-09-03 15:29:30 +0200

    Fix:handling deployment issue - Routing issue

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,7 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
 
+
 export default clerkMiddleware((auth, req) => {
     if (!isPublicRoute(req)) {
       auth().protect();
@@ -9,10 +10,13 @@ export default clerkMiddleware((auth, req) => {
 });
 
 export const config = {
+  authorizedParties: ["https://custom.vercel.app", "http://localhost:3000"],
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes
+
     "/(api|trpc)(.*)",
   ],
 };
+
